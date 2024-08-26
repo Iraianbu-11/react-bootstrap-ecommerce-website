4c6bff448d0516077a37a42615fbdc79c2812242
Iraianbu-11 <iraianbu011@gmail.com>
2024-08-26 22:59:36 +0530

    fix: cors origin error

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require('dotenv').config(); 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+  }));
 app.use(bodyParser.json());
 let amount = 0;
 
